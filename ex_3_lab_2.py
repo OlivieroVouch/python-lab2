3d68c34a97ad6b10ef045837e99c85954234e681
identifier <name@github.com>
2018-04-23 11:34:00 +0200

    end program

--- ex_3_lab_2.py
+++ ex_3_lab_2.py
@@ -2,22 +2,40 @@ from sys import argv
 script, first = argv
 print("The file is called",script)
 print("The parameter is",first)
-txt = open(first,"r")
+txt = open(first,"r") #opens file with name of "test.txt"
 task = []
-task = txt.readlines()  # read task_list and put it in a string 'task'
-txt.close()  # close FILE
-task = [line.rstrip('\n') for line in task] # read the string and clean it from newlines
+for line in txt:
+    task.append(line)
+print(task)
 index=0
-boolean=0
+boolean=False
+tasks,word=''
+
+def sub_string_remove(word):
+    delete=0
+    count=0
+    boolean=False
+    for tasks in task:
+        word = tasks.split('')
+        if word.
 
 
-while index!=4 :
+
+
+    if(delete==0): print("The indicated substring hasn't been found in any Task")
+
+    else: print("Removed" + str(delete) + "Tasks")
+
+
+
+while index!=5 :
 
     print("MENU:")
     print("1: Insert a new task")
     print("2: Remove a task")
     print("3: Show all the tasks")
-    print("4: Close the program")
+    print("4: Remove All Task containing a specified substring")
+    print("5: Close the program")
     print(" CHOOSE AN ACTION")
     index = int(input())
 
@@ -38,7 +56,16 @@ while index!=4 :
     elif index==3:
         for tasks in task:
             print(tasks)
+
     elif index==4:
+        tmp = input()
+        if len(tmp)==0:
+            print("Invalid substring")
+        else:
+            sub_string_remove(str(tmp))
+
+
+    elif index==5:
         break
     else:
         print("Invalid choice ==> REPEAT")
